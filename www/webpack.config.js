cfb776608541a82c419ca8284d56b73957522789
Rafael Briceño <rafaelbm9126@gmail.com>
2020-10-16 13:20:25 -0500

    feat: init

---
+++ www/webpack.config.js
@@ -0,0 +1,18 @@
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const path = require('path');
+
+module.exports = {
+  entry: "./bootstrap.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bootstrap.js",
+  },
+  mode: "development",
+  devServer: {
+    host: "0.0.0.0",
+    port: 8080,
+  },
+  plugins: [
+    new CopyWebpackPlugin(['index.html'])
+  ],
+};
